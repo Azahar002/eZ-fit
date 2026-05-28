763cb7d6e03f7c9274a4388767ae78050df84211
Azahar <Azahar@ml.com>
2026-05-28 15:02:43 -0400

    Stage 4: dockerize Flask app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
@@ -59,4 +60,5 @@ def delete_workout(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
